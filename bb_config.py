c685f3647f5b1300c69c4ca1b9603f79d374744d
fmixolydian <fmixolydian@proton.me>
2025-04-29 19:28:39 +0200

    added logging

--- bb_config.py
+++ bb_config.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, redirect, request, jsonify, make_response
 import sqlite3
 import json
+import os
 from werkzeug.middleware.proxy_fix import ProxyFix
+import logging
 
 def bb_validate_config(config):
 	if not ('db' in config):
 		raise Exception("invalid config ('db' missing)")
 	return config
 
-app = Flask(__name__, static_url_path='',
-                      static_folder='public',
-                      template_folder='templates')
-
 with open('config.json') as fp:
 	CONFIG = json.load(fp)
 	CONFIG = bb_validate_config(CONFIG)
+	if CONFIG['error_log']:
+		logging.basicConfig(filename = CONFIG['error_log'], level=logging.DEBUG)
+
+app = Flask(__name__, static_url_path='',
+                      static_folder='public',
+                      template_folder='templates')

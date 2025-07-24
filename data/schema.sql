24d21b07b7e3008aa525eaa50865230a44d5918f
fmixolydian <fmixolydian@proton.me>
2025-07-24 22:37:59 +0200

    Return of the Beepboard

--- data/schema.sql
+++ data/schema.sql
@@ -1,8 +1,6 @@
-PRAGMA foreign_keys = ON;
+PRAGMA foreign_keys=OFF;
 
-BEGIN TRANSACTION;
-
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
 	userid      INTEGER PRIMARY KEY AUTOINCREMENT,
 	
 	followers    INTEGER NOT NULL DEFAULT 0,
@@ -11,63 +9,70 @@ CREATE TABLE IF NOT EXISTS users (
 	downloads    INTEGER NOT NULL DEFAULT 0,
 	profileviews INTEGER NOT NULL DEFAULT 0,
 	
-	ismod        INTEGER NOT NULL DEFAULT 0,
+	rank        INTEGER NOT NULL DEFAULT 0,
 	
 	bio         TEXT NOT NULL DEFAULT '',
 	username    TEXT NOT NULL,
 	password    TEXT NOT NULL,
 	token       TEXT,
 	
-	pfp         TEXT NOT NULL DEFAULT '/assets/default-pfp.png',
-	
-	timestamp   INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS songs (
-	songid      INTEGER PRIMARY KEY AUTOINCREMENT,
-	userid      INTEGER NOT NULL,
-	
-	views       INTEGER NOT NULL DEFAULT 0,
-	likes       INTEGER NOT NULL DEFAULT 0,
-	downloads   INTEGER NOT NULL DEFAULT 0,
-	deleted     INTEGER NOT NULL DEFAULT 0,
-	featured    INTEGER NOT NULL DEFAULT 0,
-	
-	songdata    TEXT NOT NULL,
-	songmod     TEXT NOT NULL,
-	tags        TEXT NOT NULL,
-	name        TEXT NOT NULL,
-	description TEXT NOT NULL,
-	
 	timestamp   INTEGER NOT NULL,
-	
-	FOREIGN KEY (userid) REFERENCES users
+	country TEXT NOT NULL DEFAULT '',
+	discordhandle TEXT,
+	pfp TEXT NOT NULL DEFAULT 'ae3728e6-3a0a-41ce-99fa-3b6650518de7',
+	isveteran INT NOT NULL DEFAULT 0,
+	lastlogin INT
 );
 
-CREATE TABLE IF NOT EXISTS comments (
-	commentid INTEGER PRIMARY KEY AUTOINCREMENT,
-	
-	userid    INTEGER NOT NULL,
-	songid    INTEGER NOT NULL,
-	content   TEXT    NOT NULL,
-	
-	timestamp INTEGER NOT NULL,
-	
-	FOREIGN KEY (userid) REFERENCES users,
-	FOREIGN KEY (songid) REFERENCES songs
+CREATE TABLE IF NOT EXISTS "songs" (
+	"songid"	INTEGER,
+	"userid"	INTEGER NOT NULL,
+	"views"	INTEGER NOT NULL DEFAULT 0,
+	"likes"	INTEGER NOT NULL DEFAULT 0,
+	"downloads"	INTEGER NOT NULL DEFAULT 0,
+	"deleted"	INTEGER NOT NULL DEFAULT 0,
+	"featured"	INTEGER NOT NULL DEFAULT 0,
+	"songdata"	TEXT NOT NULL,
+	"songmod"	TEXT NOT NULL,
+	"tags"	TEXT NOT NULL,
+	"name"	TEXT NOT NULL,
+	"description"	TEXT NOT NULL,
+	"timestamp"	INTEGER NOT NULL, songtype TEXT NOT NULL DEFAULT 'original', remixof  INT,
+	PRIMARY KEY("songid" AUTOINCREMENT)
 );
 
-CREATE TABLE IF NOT EXISTS interactions (
-	interactionid INTEGER PRIMARY KEY AUTOINCREMENT,
-	
-	userid        INTEGER NOT NULL,
-	songid        INTEGER NOT NULL,
-	type          TEXT    NOT NULL,
-	
-	timestamp     INTEGER NOT NULL,
-	
-	FOREIGN KEY (userid) REFERENCES users,
-	FOREIGN KEY (songid) REFERENCES songs
+CREATE TABLE IF NOT EXISTS "interactions" (
+	"interactionid"	INTEGER,
+	"userid"	INTEGER NOT NULL,
+	"songid"	INTEGER NOT NULL,
+	"type"	TEXT NOT NULL,
+	"timestamp"	INTEGER NOT NULL,
+	PRIMARY KEY("interactionid" AUTOINCREMENT),
+	FOREIGN KEY("songid") REFERENCES "songs"
 );
 
-COMMIT;
+CREATE TABLE IF NOT EXISTS "comments" (
+	"commentid"	INTEGER,
+	"userid"	INTEGER NOT NULL,
+	"songid"	INTEGER NOT NULL,
+	"content"	TEXT NOT NULL,
+	"timestamp"	INTEGER NOT NULL,
+	"parent"	INTEGER,
+	PRIMARY KEY("commentid" AUTOINCREMENT),
+	FOREIGN KEY("songid") REFERENCES "songs"
+);
+
+CREATE TABLE playlists (
+	playlistid	INTEGER PRIMARY KEY AUTOINCREMENT,
+	userid	INTEGER NOT NULL,
+	name	TEXT NOT NULL,
+	timestamp INT NOT NULL,
+	FOREIGN KEY(userid) REFERENCES users
+);
+
+CREATE TABLE playlist_songs (
+	playlistid	INTEGER,
+	songid	INTEGER, timestamp INT NOT NULL DEFAULT 0,
+	FOREIGN KEY(songid) REFERENCES "songs",
+	FOREIGN KEY(playlistid) REFERENCES "playlists"
+);

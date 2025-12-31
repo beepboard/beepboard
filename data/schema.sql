c0dfb0da628ef5898c74465f66c5498bf6417ea6
fmixolydian <fmixolydian@proton.me>
2025-12-31 15:29:57 +0100

    synchronize schema.sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,30 +1,6 @@
 PRAGMA foreign_keys=OFF;
 
-CREATE TABLE users (
-	userid      INTEGER PRIMARY KEY AUTOINCREMENT,
-	
-	followers    INTEGER NOT NULL DEFAULT 0,
-	views        INTEGER NOT NULL DEFAULT 0,
-	likes        INTEGER NOT NULL DEFAULT 0,
-	downloads    INTEGER NOT NULL DEFAULT 0,
-	profileviews INTEGER NOT NULL DEFAULT 0,
-	
-	rank        INTEGER NOT NULL DEFAULT 0,
-	
-	bio         TEXT NOT NULL DEFAULT '',
-	username    TEXT NOT NULL,
-	password    TEXT NOT NULL,
-	token       TEXT,
-	
-	timestamp   INTEGER NOT NULL,
-	country TEXT NOT NULL DEFAULT '',
-	discordhandle TEXT,
-	pfp TEXT NOT NULL DEFAULT 'ae3728e6-3a0a-41ce-99fa-3b6650518de7',
-	isveteran INT NOT NULL DEFAULT 0,
-	lastlogin INT
-);
-
-CREATE TABLE songs (
+CREATE TABLE IF NOT EXISTS "songs" (
 	"songid"	INTEGER,
 	"userid"	INTEGER NOT NULL,
 	"views"	INTEGER NOT NULL DEFAULT 0,
@@ -41,7 +17,7 @@ CREATE TABLE songs (
 	PRIMARY KEY("songid" AUTOINCREMENT)
 );
 
-CREATE TABLE interactions (
+CREATE TABLE IF NOT EXISTS "interactions" (
 	"interactionid"	INTEGER,
 	"userid"	INTEGER NOT NULL,
 	"songid"	INTEGER NOT NULL,
@@ -51,7 +27,7 @@ CREATE TABLE interactions (
 	FOREIGN KEY("songid") REFERENCES "songs"
 );
 
-CREATE TABLE comments (
+CREATE TABLE IF NOT EXISTS "comments" (
 	"commentid"	INTEGER,
 	"userid"	INTEGER NOT NULL,
 	"songid"	INTEGER NOT NULL,
@@ -62,17 +38,39 @@ CREATE TABLE comments (
 	FOREIGN KEY("songid") REFERENCES "songs"
 );
 
-CREATE TABLE playlists (
-	playlistid	INTEGER PRIMARY KEY AUTOINCREMENT,
-	userid	INTEGER NOT NULL,
-	name	TEXT NOT NULL,
-	timestamp INT NOT NULL,
-	FOREIGN KEY(userid) REFERENCES users
+CREATE TABLE IF NOT EXISTS "playlist_songs" (
+	"playlistid"	INTEGER,
+	"songid"	INTEGER,
+	"timestamp"	INT NOT NULL DEFAULT 0,
+	FOREIGN KEY("songid") REFERENCES "songs"
 );
 
-CREATE TABLE playlist_songs (
-	playlistid	INTEGER,
-	songid	INTEGER, timestamp INT NOT NULL DEFAULT 0,
-	FOREIGN KEY(songid) REFERENCES "songs",
-	FOREIGN KEY(playlistid) REFERENCES "playlists"
+CREATE TABLE IF NOT EXISTS "playlists" (
+	"playlistid"	INTEGER,
+	"userid"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"timestamp"	INT NOT NULL,
+	PRIMARY KEY("playlistid" AUTOINCREMENT),
+	FOREIGN KEY("userid") REFERENCES "users"
+);
+
+CREATE TABLE IF NOT EXISTS "users" (
+	"userid"	INTEGER,
+	"followers"	INTEGER NOT NULL DEFAULT 0,
+	"views"	INTEGER NOT NULL DEFAULT 0,
+	"likes"	INTEGER NOT NULL DEFAULT 0,
+	"downloads"	INTEGER NOT NULL DEFAULT 0,
+	"profileviews"	INTEGER NOT NULL DEFAULT 0,
+	"rank"	INTEGER NOT NULL DEFAULT 0,
+	"bio"	TEXT NOT NULL DEFAULT '',
+	"username"	TEXT NOT NULL UNIQUE,
+	"password"	TEXT NOT NULL,
+	"token"	TEXT,
+	"timestamp"	INTEGER NOT NULL,
+	"country"	TEXT NOT NULL DEFAULT '',
+	"discordhandle"	TEXT,
+	"pfp"	TEXT NOT NULL DEFAULT 'ae3728e6-3a0a-41ce-99fa-3b6650518de7',
+	"isveteran"	INT NOT NULL DEFAULT 0,
+	"lastlogin"	INT,
+	PRIMARY KEY("userid" AUTOINCREMENT)
 );
